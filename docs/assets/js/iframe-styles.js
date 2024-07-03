112a9c38f718794598208ef048cccc47b5172ca7
ausdruckwebsite <167768912+ausdruckwebsite@users.noreply.github.com>
2024-07-03 16:57:45 +0200

    all das von eben

--- docs/assets/js/iframe-styles.js
+++ docs/assets/js/iframe-styles.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Check if the page is inside an iframe
     if (window !== window.top) {
         var backButton = document.getElementById('back-button');
+        var modal = document.getElementById('fullscreen-modal');
         if (backButton) {
             backButton.style.display = 'none'; // Verstecke den Button im iFrame
+        }
+        if (modal) {
+            modal.style.display = 'none'; // Verstecke das Modal im iFrame
         }
     }
 });
--- docs/media/photo_batumi1.md
+++ docs/media/photo_batumi1.md
@@ -3,7 +3,7 @@ layout: image_fullscreen
 title: Batumi 1
 permalink: /media/photo_batumi1
 image_src: http://bennettcreations.de/files/bennettcreations/photos/Georgia%20Batumi%202024/IMG09438sm.jpg
-back_link: /batumi/boom-town
+back_link: /batumi/boom-town#iframe-container
 published: true
 ---
 
